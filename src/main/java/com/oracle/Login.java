6146e144863caba0c6162f866241b8ff641e7b4f
ryosuke shimomura <shimosuk.shimosuke@gmail.com>
2014-03-15 02:17:53 +0900

    login fxml

--- src/main/java/com/oracle/Login.java
+++ src/main/java/com/oracle/Login.java
@@ -1,24 +1,14 @@
 package com.oracle;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 public class Login extends Application {
     Logger logger = LoggerFactory.getLogger(Login.class);
@@ -28,58 +18,14 @@ public class Login extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("JavaFX Welcome");
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
 
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
+        Scene scene = new Scene(root, 300, 275);
 
-        Text scenetitle = new Text("Welcome");
-        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-        grid.add(scenetitle, 0, 0, 2, 1);
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
 
-        Label userName = new Label("User Name:");
-        grid.add(userName, 0, 1);
-
-        final TextField userTextField = new TextField();
-        grid.add(userTextField, 1, 1);
-
-        Label pw = new Label("Password:");
-        grid.add(pw, 0, 2);
-
-        final PasswordField pwBox = new PasswordField();
-        grid.add(pwBox, 1, 2);
-
-        Button btn = new Button("Sign in");
-        HBox hbBtn = new HBox(10);
-        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-        hbBtn.getChildren().add(btn);
-        grid.add(hbBtn, 1, 4);
-
-        final Text actiontarget = new Text();
-        grid.add(actiontarget, 1, 6);
-
-        scenetitle.setFill(Color.FIREBRICK);
-        scenetitle.setId("welcome-text");
-        actiontarget.setId("actiontarget");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent e) {
-                logger.info("Sign In : name[{}], pass[{}]", userTextField.getText(), pwBox.getText());
-                actiontarget.setFill(Color.FIREBRICK);
-                actiontarget.setText("Sign in button pressed");
-            }
-        });
-
-        Scene scene = new Scene(grid, 300, 275);
-        primaryStage.setScene(scene);
-
-        scene.getStylesheets().add(Login.class.getResource("Login.css").toExternalForm());
-
-        primaryStage.show();
     }
 }
