5d6746e9b16b3eede9de08378ed1a74fcf09ee59
ryosuke shimomura <shimosuk.shimosuke@gmail.com>
2014-03-15 15:32:19 +0900

    add js file

--- src/main/java/com/oracle/LoginController.java
+++ src/main/java/com/oracle/LoginController.java
@@ -2,12 +2,20 @@ package com.oracle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LoginController {
+    Logger logger = LoggerFactory.getLogger(LoginController.class);
     @FXML private Text actiontarget;
+    @FXML private TextField nameField;
+    @FXML private PasswordField passwordField;
 
     @FXML protected void handleSubmitButtonAction(ActionEvent event) {
+        logger.info("name[password] : {}[{}]", nameField.getText(), passwordField.getText());
         actiontarget.setText("Sign in button pressed");
     }
 }
